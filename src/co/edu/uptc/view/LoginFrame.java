b03b57bd2aa5c1a7f57d4cd17c34a5e8df970e47
Carlos Arenas <micorreo@uptc.edu.co>
2025-03-23 18:41:19 -0500

    first commit

---
+++ src/co/edu/uptc/view/LoginFrame.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.view;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class LoginFrame extends JFrame {
+    public LoginFrame() {
+        super("Login Frame");
+        setSize(800, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
