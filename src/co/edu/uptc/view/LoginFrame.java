d5d187b631ed3ca44d04818583742cc79d6344c2
Carlos Arenas <micorreo@uptc.edu.co>
2025-03-23 18:50:08 -0500

    Primer cambio

--- src/co/edu/uptc/view/LoginFrame.java
+++ src/co/edu/uptc/view/LoginFrame.java
@@ -2,12 +2,20 @@ package co.edu.uptc.view;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import java.awt.event.*;
 
-public class LoginFrame extends JFrame {
+public class LoginFrame extends JFrame implements ActionListener {
     public LoginFrame() {
         super("Login Frame");
         setSize(800, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 }
